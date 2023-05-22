886ae889cd554777cc03358f286a56b2f7adb876
duyanh3110 <duy.nguyen@sarake.fi>
2023-05-22 20:30:46 +0300

    Config project

---
+++ src/configs/config.mongodb.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// level 0
+// const config = {
+//   app: {
+//     port: 3000,
+//   },
+//   db: {
+//     host: "localhost",
+//     port: 27017,
+//     name: "db",
+//   },
+// };
+
+// level 1
+// const dev = {
+//   app: {
+//     port: 3000,
+//   },
+//   db: {
+//     host: "localhost",
+//     port: 27017,
+//     name: "dbDev",
+//   },
+// };
+
+// const prod = {
+//   app: {
+//     port: 3000,
+//   },
+//   db: {
+//     host: "localhost",
+//     port: 27017,
+//     name: "dbProd",
+//   },
+// };
+
+//level 2
+const dev = {
+  app: {
+    port: process.env.DEV_APP_PORT || 3000,
+  },
+  db: {
+    host: process.env.DEV_DB_HOST || "localhost",
+    port: process.env.DEV_DB_PORT || 27017,
+    name: process.env.DEV_DB_NAME || "dbDev",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    url: process.env.DB_URL,
+  },
+};
+
+const prod = {
+  app: {
+    port: process.env.PROD_APP_PORT || 3000,
+  },
+  db: {
+    host: process.env.PROD_DB_HOST || "localhost",
+    port: process.env.PROD_DB_PORT || 27017,
+    name: process.env.PROD_DB_NAME || "dbProd",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    url: process.env.DB_URL,
+  },
+};
+
+const config = { dev, prod };
+const env = process.env.NODE_ENV || "dev";
+module.exports = config[env];
---
+++ src/dbs/init.mongodb.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+// const {
+//   db: { host, name, port },
+// } = require("../configs/config.mongodb");
+// const connectString = `mongodb://${host}:${port}/${name}`;
+
+const {
+  db: { user, password, url },
+} = require("../configs/config.mongodb");
+const connectString = `mongodb+srv://${user}:${password}@${url}`;
+const { countConnect } = require("../helpers/check.connect");
+
+console.log(`connectString: ${connectString}`);
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  connect(type = "mongodb") {
+    // Set when env = dev
+    mongoose.set("debug", true);
+    mongoose.set("debug", { color: true });
+
+    mongoose
+      .connect(connectString)
+      .then((_) => {
+        console.log(`Connected Mongodb Success PRO`, countConnect());
+      })
+      .catch((err) => console.log("Error connecting"));
+  }
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+
+    return Database.instance;
+  }
+}
+
+const instanceMongodb = Database.getInstance();
+
+module.exports = instanceMongodb;
