8539e171115792a6d3b386b58e59ec9f33063a0e
duyanh3110 <duyanh19962012@gmail.com>
2023-09-14 15:46:05 +0300

    Handle error & success response

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -6,6 +6,7 @@ const shopModel = require("../models/shop.model");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const { BadRequestError } = require("../core/error.response");
 
 const ShopRole = {
   SHOP: "SHOP",
@@ -16,91 +17,84 @@ const ShopRole = {
 
 class AccessService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      // Step 1: Check email exists??
-      const holderShop = await shopModel.findOne({ email }).lean();
-      if (holderShop) {
-        return {
-          code: "xxxx",
-          message: "Shop already exists!",
-        };
-      }
+    // try {
+    // Step 1: Check email exists??
+    const holderShop = await shopModel.findOne({ email }).lean();
+    if (holderShop) {
+      throw new BadRequestError("Error: Shop already registered!");
+    }
 
-      const passwordHash = await bcrypt.hash(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
 
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: passwordHash,
-        roles: [ShopRole.SHOP],
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: passwordHash,
+      roles: [ShopRole.SHOP],
+    });
+
+    if (newShop) {
+      // Create privateKey, publicKey
+      // privateKey:: sign token
+      // publicKey:: verify token
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      //   modulusLength: 4096,
+      //   publicKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      //   privateKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      // });
+
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
+      console.log({ privateKey, publicKey }); // TODO: save collection KeyStore
+
+      const keyStore = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
       });
 
-      if (newShop) {
-        // Create privateKey, publicKey
-        // privateKey:: sign token
-        // publicKey:: verify token
-        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        //   modulusLength: 4096,
-        //   publicKeyEncoding: {
-        //     type: "pkcs1",
-        //     format: "pem",
-        //   },
-        //   privateKeyEncoding: {
-        //     type: "pkcs1",
-        //     format: "pem",
-        //   },
-        // });
-
-        const privateKey = crypto.randomBytes(64).toString("hex");
-        const publicKey = crypto.randomBytes(64).toString("hex");
-        console.log({ privateKey, publicKey }); // TODO: save collection KeyStore
-
-        const keyStore = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-
-        if (!keyStore) {
-          return {
-            code: "xxxx",
-            message: "keyStore error",
-          };
-        }
-
-        // Create token paired
-        const tokens = await createTokenPair(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey,
-        );
-        console.log(`Created Token Success::`, tokens);
-
-        return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({
-              fields: ["_id", "name", "email"],
-              object: newShop,
-            }),
-            shop: newShop,
-            tokens,
-          },
-        };
+      if (!keyStore) {
+        throw new BadRequestError("Error: Keystore error!");
       }
 
+      // Create token paired
+      const tokens = await createTokenPair(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey,
+      );
+
       return {
-        code: 200,
-        metadata: null,
-      };
-    } catch (error) {
-      console.log(error);
-      return {
-        code: "xxx",
-        message: error.message,
-        status: "error",
+        code: 201,
+        metadata: {
+          shop: getInfoData({
+            fields: ["_id", "name", "email"],
+            object: newShop,
+          }),
+          shop: newShop,
+          tokens,
+        },
       };
     }
+
+    return {
+      code: 200,
+      metadata: null,
+    };
+    // } catch (error) {
+    //   console.log(error);
+    //   return {
+    //     code: "xxx",
+    //     message: error.message,
+    //     status: "error",
+    //   };
+    // }
   };
 }
 
